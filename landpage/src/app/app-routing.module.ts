1a10fe8291ac541a4d30436251455cff31843c16
psedonymous <psedonymous@github.com>
2019-01-05 13:09:20 +0530

    On branch Login

--- landpage/src/app/app-routing.module.ts
+++ landpage/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { NavComponent } from './nav/nav.component'
+import { AboutComponent } from './about/about.component'
+import { ContactComponent } from './contact/contact.component'
+import { LoginComponent } from './login/login.component'
+/*import { SigninComponent } from './signin/signin.component'
+*/
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'nav', component:NavComponent},
+  {path: 'contact', component:ContactComponent},
+/*  {path: 'signin', component:SigninComponent},
+  */{path: 'login', component:LoginComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- landpage/src/app/app.module.ts
+++ landpage/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
